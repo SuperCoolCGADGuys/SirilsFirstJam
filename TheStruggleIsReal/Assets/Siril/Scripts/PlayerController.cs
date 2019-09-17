308652f8ad872b2114be6827f53b13a95756b206
Siril Sunny <siril.sunny@yahoo.com>
2019-09-17 20:50:59 +0100

    Player Movement and Shooting

---
+++ TheStruggleIsReal/Assets/Siril/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    //handling variables:
+    public float rotationSpeed = 450;
+    public float walkSpeed = 5;
+    public float runSpeed = 8;
+    
+    //system:
+    private Quaternion targetRotation;
+
+    //Components:
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0 );
+
+        if (input != Vector3.zero)
+        {
+            targetRotation = Quaternion.LookRotation(input);
+            transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.z, targetRotation.eulerAngles.z, rotationSpeed * Time.deltaTime);
+
+        }
+
+    }
+}
