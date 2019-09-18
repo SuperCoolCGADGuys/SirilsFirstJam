968e76dee6177f9cac3cd23e262bdb170373ce03
Aidan Murray <1702270@uad.ac.uk>
2019-09-18 20:36:22 +0100

    Player health stuff

---
+++ TheStruggleIsReal/Assets/Aidan/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	[SerializeField] private Transform target;
+	[SerializeField] private float smoothTime = 0.125f;
+	[SerializeField] private Vector3 offset = Vector2.zero;
+	private Vector3 velocity = Vector3.one;
+
+	// Update is called once per frame
+	void LateUpdate()
+	{
+		Vector3 desiredPos = target.position + offset;
+		Vector3 smoothedPos = Vector3.SmoothDamp(transform.position, desiredPos, ref velocity, smoothTime);
+		transform.position = smoothedPos;
+	}
+}
---
+++ TheStruggleIsReal/Assets/Aidan/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+	[SerializeField] private int maxHealth = 4;
+	[SerializeField] private float invincibilityTime = 2f;
+	[SerializeField] SpriteRenderer spriteRenderer;
+	[SerializeField] private float flashLength = 1f;
+	private float elapsedInvincibilityTime = 0;
+	private float flashCounter;
+	private int currentHealth;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		currentHealth = maxHealth;
+	}
+
+	private void Update()
+	{
+		// If the player has been hit enable invincibility time
+		if (elapsedInvincibilityTime > 0)
+		{
+			elapsedInvincibilityTime -= Time.deltaTime;
+			flashCounter -= Time.deltaTime;
+
+			if (flashCounter <= 0)
+			{
+				spriteRenderer.enabled = !spriteRenderer.enabled;
+				flashCounter = flashLength;
+			}
+
+			if (elapsedInvincibilityTime <= 0)
+			{
+				spriteRenderer.enabled = true;
+			}
+		}
+	}
+
+	public void Damage(int amount, Vector2 knockBackDir, float knockBackForce, float knockBackTime)
+	{
+		if (elapsedInvincibilityTime <= 0)
+		{
+			if ((currentHealth - amount) <= 0)
+			{
+				// Dead
+				currentHealth = 0;
+				Kill();
+			}
+			else
+			{
+				currentHealth -= amount;
+			}
+			//gameObject.GetComponent<PlayerController>().KnockBack(knockBackDir, knockBackForce, knockBackTime);
+			elapsedInvincibilityTime = invincibilityTime;
+			flashCounter = flashLength;
+
+			// Shake the screen
+			CameraShaker.Instance.ShakeOnce(4f, 4f, .2f, .1f);
+		}
+	}
+
+	private void Kill()
+	{
+		// Play death animation sound and particles
+	}
+}
